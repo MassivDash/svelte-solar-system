76b6b1bce87842a5418f86d72ba0bb6e8dc1e10c
Lukasz Celitan <lukasz.celitan@gmail.com>
2021-03-02 18:40:11 +0100

    working footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -12,6 +12,7 @@ p {
     color: var(--black);
     font-size: 14px;
     margin-left: 10px;
+    margin-top: 5px;
 }
 
 @media screen and (min-width: 1000px){
@@ -19,14 +20,22 @@ p{ font-size: 15px;
     margin-top: 10px;
     margin-left: 20px; 
 }
+
+a {
+    color: var(--prime);
+    text-decoration: none;
+}
+
 }
 
 
 </style>
 
 <footer>
-    <p>Designed with &#10084; by Spaceout.pl</p>
-    <p>Made with Svelte framework</p>
+    <p>Designed with &#10084; by <a href="https://spaceout.pl/"> Spaceout.pl</a></p>
+    <p>Made with <a href="https://svelte.dev/" target="_blank">Svelte framework</a></p>
+    <p>Source code at <a href="https://github.com/MassivDash/svelte-solar-system" target="_blank"> github </a></p>
+    <p>Inspired by <a href="https://codepen.io/robdimarzo/pen/LMOLer" target="_blank">Rob DiMarzo</a> and <a href="https://codepen.io/juliangarnier/pen/idhuG">Julian Garnier</a></p>
 
 
 </footer>

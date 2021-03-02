3dceeda5db758e731d86951178574a0505938ad9
Lukasz Celitan <lukasz.celitan@gmail.com>
2021-03-02 18:27:00 +0100

    working footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,32 @@
 <style>
 footer {
+  display: block;  
   background: white;
+  width: 100%;
+  min-height:150px;
+  padding: 10px;
 }
+
+p {
+    text-align: 'center';
+    color: var(--black);
+    font-size: 14px;
+    margin-left: 10px;
+}
+
+@media screen and (min-width: 1000px){
+p{ font-size: 15px;
+    margin-top: 10px;
+    margin-left: 20px; 
+}
+}
+
+
 </style>
 
-<footer>Designed by Spaceout.pl</footer>
+<footer>
+    <p>Designed with &#10084; by Spaceout.pl</p>
+    <p>Made with Svelte framework</p>
+
+
+</footer>
--- src/components/solarInfo.svelte
+++ src/components/solarInfo.svelte
@@ -1,11 +1,4 @@
 <style>
-.flex-holder {
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  padding-bottom: 100px;
-  margin-top: 100px;
-}
 
 h3 {
   font-size: 25px;
@@ -31,31 +24,32 @@ p {
 </style>
 
 <script lang="ts">
-import IntersectionObserver from "./UI/intersectionObserver.svelte";
-</script>
 
-<IntersectionObserver let:intersecting top="{-400}">
-  <div class="flex-holder">
-    <div class="{`text-container ${intersecting ? 'show' : 'hide'}`}">
-      <h3>Birth of the Solar System</h3>
-      <p>
-        Our solar system began as part of a massive nebula cloud of molecular
-        hydrogen and dust around 4.6 billion years ago. In a region of this dark
-        cloud conditions allowed gravity to begin condensing the hydrogen until
-        a substantial mass began to grow ever larger and hotter, eventually this
-        mass collapsed in on itself, forming the early stage of a star called a
-        protostar. The gravitational pull of the embryonic star caused a large
-        disc of gas and dust to begin forming around it.
-      </p>
-      <p>
-        Over millions of years pressure began to build up inside the protostar
-        as it became hotter and denser until eventually nuclear fusion began in
-        its core, giving birth to the sun as we know it. During this time the
-        disc of gas and dust that had formed around the star had also began
-        condensing into ever growing orbiting bodies. In time they would go on
-        to form the planets, moons and other objects in our solar system.
-      </p>
-      <div class="spacer"></div>
-    </div>
+import IntersectionObserver from "./UI/intersectionObserver.svelte";
+
+</script>
+<IntersectionObserver let:intersecting top={-400}>
+<div class="flex-holder">
+  <div class={`text-container ${intersecting ? 'show' : 'hide'}`}>
+    <h3>Birth of the Solar System</h3>
+    <p>
+      Our solar system began as part of a massive nebula cloud of molecular
+      hydrogen and dust around 4.6 billion years ago. In a region of this dark
+      cloud conditions allowed gravity to begin condensing the hydrogen until a
+      substantial mass began to grow ever larger and hotter, eventually this
+      mass collapsed in on itself, forming the early stage of a star called a
+      protostar. The gravitational pull of the embryonic star caused a large
+      disc of gas and dust to begin forming around it.
+    </p>
+    <p>
+      Over millions of years pressure began to build up inside the protostar as
+      it became hotter and denser until eventually nuclear fusion began in its
+      core, giving birth to the sun as we know it. During this time the disc of
+      gas and dust that had formed around the star had also began condensing
+      into ever growing orbiting bodies. In time they would go on to form the
+      planets, moons and other objects in our solar system.
+    </p>
+    <div class="spacer"></div>
   </div>
+</div>
 </IntersectionObserver>
